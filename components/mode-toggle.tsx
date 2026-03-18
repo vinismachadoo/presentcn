93c2732843068dad226a58dc0e7dd8a8924279ec
Vinicius Sanches <vinicius.sanches@poli.ufrj.br>
2026-03-18 00:40:17 -0300

    fix mode toggle

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -40,9 +40,11 @@ const ModeToggle = ({
         <Moon className="block size-4 dark:hidden" />
         <span className="sr-only">Toggle theme</span>
       </TooltipTrigger>
-      <TooltipContent className={cn(shortcut && "pe-2")}>
+      <TooltipContent
+        className={cn("flex items-center gap-2", shortcut && "pe-2")}
+      >
         Toggle Mode
-        {shortcut && <Kbd>{shortcut}</Kbd>}
+        {shortcut && <Kbd className="uppercase">{shortcut}</Kbd>}
       </TooltipContent>
     </Tooltip>
   )
